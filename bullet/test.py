731ee4de5b5761e6f7436e64f3b7e2e23076eec9
Ryan McCartney <rmccartney856@gmail.com>
2019-06-24 14:15:42 +0100

    URDF Update

--- bullet/test.py
+++ bullet/test.py
@@ -11,7 +11,7 @@ p.setGravity(0,0,-10)
 #planeId = p.loadURDF("plane.urdf")
 cubeStartPos = [0,0,1]
 cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
-boxId = p.loadURDF("r2d2.urdf",cubeStartPos, cubeStartOrientation)
+boxId = p.loadURDF("urdf/ar2.urdf",cubeStartPos, cubeStartOrientation)
 p.stepSimulation()
 cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
 print(cubePos,cubeOrn)
